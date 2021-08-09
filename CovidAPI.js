d511d06546e45dbc797d35dcb5fe162e8bb39e78
Sharan Raj <you@example.com>
2021-08-10 04:53:17 +1200

    First commit for Task01

--- CovidAPI.js
+++
@@ -1,9 +0,0 @@
-var req = new XMLHttpRequest();
-req.open('GET','https://api.covid19api.com/summary',true);
-req.send();
-req.onload=function(){
-    var data=JSON.parse(this.response);
-    for (var i in data){
-        console.log(data["Global"]);
-    }
-}
